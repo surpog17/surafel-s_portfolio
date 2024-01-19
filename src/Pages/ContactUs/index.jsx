d0834094bc4d4ea65d90d8ba6d025655c394ad56
surpog17 <surafelkifle17@gmail.com>
2024-01-19 22:09:29 +0300

    contact me

--- src/Pages/ContactUs/index.jsx
+++ src/Pages/ContactUs/index.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { motion } from "framer-motion";
 import Info from "./Info";
 import Talk from "./Talk";
 
-export default function index() {
+const containerAnimation = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.8 } },
+};
+
+const Index = () => {
   return (
-    <div
+    <motion.div
       id="projects"
       className="bg-white min-h-screen px-4 md:px-8 lg:px-16 xl:px-32"
+      initial="hidden"
+      animate="visible"
+      variants={containerAnimation}
     >
       <div className="flex flex-col-reverse md:flex-row justify-evenly gap-10">
         <Info />
         <Talk />
       </div>
-    </div>
+    </motion.div>
   );
-}
+};
+
+export default Index;
