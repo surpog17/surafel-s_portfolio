641fd447d2a07655b7a5a0976f8326ba911f38e0
surpog17 <surafelkifle17@gmail.com>
2024-01-20 22:12:32 +0300

    contact nre

--- src/App.jsx
+++ src/App.jsx
@@ -46,11 +46,13 @@ export default function App() {
         <button
           title="Scroll UP"
           onClick={scrollToTop}
-          className="fixed bottom-4 right-4 border-2 border-black bg-transparent hover:bg-black text-black  hover:text-white p-2 rounded-full transition duration-1000 ease-in-out mix-blend-lighten z-[1]	"
+          className="fixed bottom-4 right-4 border-2 border-black bg-transparent hover:bg-black text-black hover:text-white p-2 rounded-full transition duration-1000 ease-in-out z-[1]"
+          style={{ mixBlendMode: "screen" }}
         >
           <FaArrowUp />
         </button>
       )}
+
       <Footer />
     </div>
   );
